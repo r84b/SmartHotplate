75762485d6ef51f6e2bcca664403a23886f81a22
RB84 <ramon@nebulae.nl>
2025-07-18 22:35:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ buzzer = Buzzer(16)
 sensors = SensorController()
 heater = HeaterControl(sensors.sensor_plate.read_temperature, buzzer)
 stirrer = StirrerController(1, 22)
-context = ProcessContext(heater, stirrer, sensor)
+display = DisplayController()  # or whatever your SH1106 wrapper is called
+context = ProcessContext(heater, stirrer, sensors)
 
 # Engine
 engine = PhaseEngine(context)
@@ -23,16 +24,20 @@ engine.add_phase(HeatTo(context, 65.0))
 engine.add_phase(HoldTemp(context, 65.0, duration=180))
 engine.add_phase(StirFor(context, rpm=500, duration=300))
 
-# Start WiFi connectie in de achtergrond
-asyncio.create_task(web_interface.connect_wifi())
-asyncio.create_task(heater.pwm_burst_loop())
-#asyncio.create_task(heater.monitor_failsafe())
+async def main():
+   # Start WiFi connectie in de achtergrond
+   asyncio.create_task(web_interface.connect_wifi())
+   asyncio.create_task(heater.pwm_burst_loop())
+   #asyncio.create_task(heater.monitor_failsafe())
 
-web_interface.set_context(context)
+   web_interface.set_context(context)
 
-# Execution loop
-while True:
-    engine.update()
-    context.update_all()
-    display.update(context)
-    sleep(0.5)
+   # Execution loop
+   while True:
+      engine.update()
+      context.update_all()
+      display.update(context)
+      await asyncio.sleep(0.5)
+       
+       
+asyncio.run(main())
