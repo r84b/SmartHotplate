d8cc94e0389c5665285fc54bf9d37b173e640ae4
RB84 <ramon@nebulae.nl>
2025-07-18 21:58:30 +0200

    Update context.py

--- context.py
+++ context.py
@@ -1,11 +1,13 @@
+from equipment.heater_control import HeaterControl
+
 class ProcessContext:
-    def __init__(self, heater, stirrer, sensor):
+    def __init__(self, heater: HeaterControl, stirrer, sensor):
         self.heater = heater
         self.stirrer = stirrer
         self.sensor = sensor
 
-    def read_temp(self):
-        return self.sensor.read_temp()
+    def set_target_temp(self, t):
+        self.heater.set_target_temp(t)
 
     def heat_on(self):
         self.heater.enable()
@@ -13,8 +15,9 @@ class ProcessContext:
     def heat_off(self):
         self.heater.disable()
 
-    def set_rpm(self, rpm):
-        self.stirrer.set_rpm(rpm)
+    def update_all(self):
+        self.heater.update()
+        # Optional: stirrer.update() if using similar closed-loop control
 
-    def stop_stirrer(self):
-        self.stirrer.stop()
+    def read_temp(self):
+        return self.sensor.read_temp()
