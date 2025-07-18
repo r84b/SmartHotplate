18da879d121d2249cfab797b705045c38329e670
RB84 <ramon@nebulae.nl>
2025-07-18 22:40:49 +0200

    Update process_context.py

--- context/process_context.py
+++ context/process_context.py
@@ -1,10 +1,12 @@
-from equipment.heater_control import HeaterControl
+from equipment.heater.control import HeaterControl
+from equipment.buzzer.control import Buzzer
 
 class ProcessContext:
-    def __init__(self, heater: HeaterControl, stirrer, sensor):
+    def __init__(self, heater: HeaterControl, stirrer, sensor, buzzer: Buzzer):
         self.heater = heater
         self.stirrer = stirrer
         self.sensor = sensor
+        self.buzzer = buzzer
 
     def set_target_temp(self, t):
         self.heater.set_target_temp(t)
@@ -17,7 +19,16 @@ class ProcessContext:
 
     def update_all(self):
         self.heater.update()
-        # Optional: stirrer.update() if using similar closed-loop control
+        # Optional: stirrer.update()
 
     def read_temp(self):
         return self.sensor.read_temp()
+
+    def buzz_target_reached(self):
+        self.buzzer.target_reached()
+
+    def buzz_alarm(self):
+        self.buzzer.alarm()
+
+    def buzz_click(self):
+        self.buzzer.click()
